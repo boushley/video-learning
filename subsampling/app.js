81b9ec157032e4d6c843e69f6a65b73d54005075
Aaron Boushley <aboushley@axon.com>
2020-02-08 11:58:30 -0800

    Get rendering of YUV planar working

--- subsampling/app.js
+++ subsampling/app.js
@@ -1,41 +1,134 @@
+const els = {
+  dropArea: document.getElementById('drop-area'),
+  sourceCanvas: document.getElementById('source-canvas'),
+  sourceContext: document.getElementById('source-canvas').getContext('2d'),
+  yPlaneCanvas: document.getElementById('y-plane-canvas'),
+  yPlaneContext: document.getElementById('y-plane-canvas').getContext('2d'),
+  uPlaneCanvas: document.getElementById('u-plane-canvas'),
+  uPlaneContext: document.getElementById('u-plane-canvas').getContext('2d'),
+  vPlaneCanvas: document.getElementById('v-plane-canvas'),
+  vPlaneContext: document.getElementById('v-plane-canvas').getContext('2d'),
+}
+
+let sourceImage = {}
+
 setupDropArea()
 
-const sourceCanvas = document.getElementById('source-canvas')
-const sourceContext = sourceCanvas.getContext('2d')
+function getYUV({r, g, b}) {
+  y = (0.299 * r) + (0.587 * g) + (0.114 * b)
+  u = (-0.147 * r) - (0.289 * g) + (0.436 * b)
+  v = (0.615 * r) - (0.515 * g) - (0.100 * b)
 
-function drawToCanvas(file) {
-  const img = new Image()
-  img.onload = () => {
-    sourceContext.drawImage(img, 0, 0)
-    URL.revokeObjectURL(img.src)
+  return {y, u, v}
+}
+
+function getRGB({y, u, v}) {
+  r = y + (1.403 * v)
+  g = y - (0.344 * u) - (0.714 * v)
+  b = y + (1.770 * u)
+
+  return {r, g, b}
+}
+
+function renderPlane(context, plane, toRGB) {
+  const planeData = context.createImageData(sourceImage.width, sourceImage.height)
+  for (let i = 0; i < sourceImage.width; i++) {
+    for (let j = 0; j < sourceImage.height; j++) {
+      const pixelNumber = (j * sourceImage.width) + i
+      const pixelStart = pixelNumber * 4
+
+      const y = plane[pixelNumber]
+      const {r, g, b} = toRGB(y)
+
+      planeData.data[pixelStart] = r
+      planeData.data[pixelStart+1] = g
+      planeData.data[pixelStart+2] = b
+      planeData.data[pixelStart+3] = 255
+    }
+  }
+  context.putImageData(planeData, 0, 0)
+}
+
+function renderPlanes() {
+  console.log('UPlane Point:', sourceImage.uPlane[(97*sourceImage.width) + 333])
+  renderPlane(els.yPlaneContext, sourceImage.yPlane, y => getRGB({ y, u: 0, v: 0 }))
+  renderPlane(els.uPlaneContext, sourceImage.uPlane, u => getRGB({ y: 128, u, v: -60 }))
+  renderPlane(els.vPlaneContext, sourceImage.vPlane, v => getRGB({ y: 128, u: -60, v }))
+}
+
+function dataReady() {
+  sourceImage.imageData = els.sourceContext.getImageData(0, 0, sourceImage.width, sourceImage.height)
+
+  const pixelCount = sourceImage.width * sourceImage.height
+  sourceImage.yPlane = new Uint8Array(pixelCount)
+  sourceImage.uPlane = new Uint8Array(pixelCount)
+  sourceImage.vPlane = new Uint8Array(pixelCount)
+
+  for (let i = 0; i < sourceImage.width; i++) {
+    for (let j = 0; j < sourceImage.height; j++) {
+      const pixelNumber = (j * sourceImage.width) + i
+      const pixelStart = pixelNumber * 4
+      const pixel = {
+        r: sourceImage.imageData.data[pixelStart],
+        g: sourceImage.imageData.data[pixelStart + 1],
+        b: sourceImage.imageData.data[pixelStart + 2],
+      }
+      const {y, u, v} = getYUV(pixel)
+      sourceImage.yPlane[pixelNumber] = y
+      sourceImage.uPlane[pixelNumber] = u
+      sourceImage.vPlane[pixelNumber] = v
+    }
   }
 
-  img.src = URL.createObjectURL(file)
+  renderPlanes()
+}
+
+function drawFileToCanvas(file) {
+  const url = URL.createObjectURL(file)
+  drawUrlToCanvas(url, () => URL.revokeObjectURL(url))
+}
+
+function drawUrlToCanvas(url, cb) {
+  els.dropArea.classList.add('hidden')
+  const img = new Image()
+  img.onload = () => {
+    if (cb) {
+      cb()
+    }
+    sourceImage.width = img.naturalWidth
+    sourceImage.height = img.naturalHeight
+    els.sourceContext.drawImage(img, 0, 0)
+
+    dataReady()
+  }
+
+  img.src = url
 }
 
 function setupDropArea () {
-  const dropArea = document.getElementById('drop-area')
-
-  dropArea.addEventListener('dragenter', highlight, false)
-  dropArea.addEventListener('dragover', highlight, false)
-  dropArea.addEventListener('dragleave', unhighlight, false)
-  dropArea.addEventListener('drop', (event) => {
+  els.dropArea.addEventListener('dragenter', highlight, false)
+  els.dropArea.addEventListener('dragover', highlight, false)
+  els.dropArea.addEventListener('dragleave', unhighlight, false)
+  els.dropArea.addEventListener('drop', (event) => {
     unhighlight(event)
     event.preventDefault()
     for (let i = 0; i < event.dataTransfer.items.length; i++) {
       if (event.dataTransfer.items[i].kind === 'file') {
         const file = event.dataTransfer.items[i].getAsFile();
-        console.log('... file[' + i + '].name = ' + file.name);
-        drawToCanvas(file);
+        sourceImage.file = file
+        drawFileToCanvas(file);
+        break;
       }
     }
   }, false)
   function highlight(e) {
-    dropArea.classList.add('highlight')
+    els.dropArea.classList.add('highlight')
     e.preventDefault()
   }
   function unhighlight(e) {
-    dropArea.classList.remove('highlight')
+    els.dropArea.classList.remove('highlight')
     e.preventDefault()
   }
 }
+
+drawUrlToCanvas('/small-tears-of-steel-frame-00829.png')
